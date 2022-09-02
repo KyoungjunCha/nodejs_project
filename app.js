a14463493406be6b99517610fbc5978c3bf42138
KyoungjunCha <58018247+KyoungjunCha@users.noreply.github.com>
2022-09-02 12:00:53 +0900

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+http.createServer((req,res) => {
+    res.writeHead(200,{'Content-Type' : 'text/html; charset=utf-8'});
+    res.write('<h1> Node.js 로 서버 만들기 </h1>');
+    res.end('<p> 첫번째 서버</p>');
+}).listen(8080,() => {
+    console.log('8080 포트 실행');
+})
